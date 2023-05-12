eb118e6ddb9a80776a6892a617edae0f89e12d02
Vitor <vitorcastanhos@gmail.com>
2023-05-12 12:15:36 -0300

    returning

--- script.js
+++ script.js
@@ -157,10 +157,4 @@ function init() {
   y += dy;
 }
 
-function is_touch_enabled() {
-  return ( 'ontouchstart' in window ) ||
-         ( navigator.maxTouchPoints > 0 ) ||
-         ( navigator.msMaxTouchPoints > 0 );
-}
-
 setInterval(init, 10);
